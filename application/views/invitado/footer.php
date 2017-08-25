f6070f385212ab2337e088408eb62046926e7a24
Cecilia Jimenez <cecyjima@gmail.com>
2017-08-25 00:32:39 -0500

    cambios en footer

--- application/views/invitado/footer.php
+++ application/views/invitado/footer.php
@@ -24,12 +24,12 @@
                 <a href="#">
                   <span class="fa-stack fa-lg">
                     <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                    <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
                   </span>
                 </a>
               </li>
             </ul>
-            <p class="copyright text-muted">Copyright &copy; Your Website 2017</p>
+            <p class="copyright text-muted">Copyright &copy; Developer for Leticia, Cecilia</p>
           </div>
         </div>
       </div>
