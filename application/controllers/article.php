fa97557bb968459af2712ec58d600c2e86c77d07
Cecilia Jimenez <cecyjima@gmail.com>
2017-08-25 00:30:51 -0500

    agregue mas cosas

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -9,7 +9,20 @@ class Article extends CI_Controller
 	{
 		$fila = $this->usuario->getUsuarioByName($id);
 
-		echo $fila->usuario;
+		$data  = array('title' => $fila->email);
+		$this->load->view("/invitado/head",$data);
+		$data = array('mensaje' => 'SPA-UAMI');
+		$this->load->view("/invitado/nav",$data);
+		$data  = array('mensaje' => $fila->email,
+			'descripcion'=> $fila->contraseña);
+		$this->load->view("/invitado/header",$data);
+
+		$data = array('contenido' => $fila->contraseña);
+		$this->load->view("/invitado/post", $data);
+
+		$this->load->view("/invitado/footer", $data);
+
+
 	}
 }
 ?>
