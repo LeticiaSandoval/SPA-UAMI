90b8af542dfe803ed082c1976535d0f9d8f483c9
Cecilia Jimenez <cecyjima@gmail.com>
2017-08-25 00:33:42 -0500

    se agrego archivo post

---
+++ application/views/invitado/post.php
@@ -0,0 +1,10 @@
+<article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <p><?= $contenido ?></p>
+
+          </div>
+        </div>
+      </div>
+    </article>
